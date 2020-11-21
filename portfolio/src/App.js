8719da9baa0e37b8cb23954f56d2d76c96145d5a
Santiago Solana <santiago.sjs@gmail.com>
2020-11-21 01:14:26 -0500

    styles

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,46 @@
 import React from "react";
-import Header from './components/Header'
-import Container from './components/Container'
+import Header from "./components/Header";
+import Container from "./components/Container";
 import About from "./Pages/About";
 import Portfolio from "./Pages/Porfolio";
-
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Particles from "react-particles-js";
 
 function App() {
   return (
-    <Container>
-      <Header/>
-      <About/>
-      <Portfolio/>
-    </Container>
+    <Router>
+      <div>
+        <Particles className="particles"
+         />
+         <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%"
+          }}
+        >
+          <Container>
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                <About />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/portfolio">
+                <Portfolio />
+              </Route>
+
+              {/* <Route component={NoMatch} /> */}
+            </Switch>
+        
+          </Container>
+          </div>
+      </div>
+    </Router>
   );
 }
 
--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,34 +1,62 @@
 import React from "react";
 import "./style.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function Header(props) {
   return (
-    <div  className="card justify-content-center header-card">
-            <div className="typewriter">
-                <h1>Hello, my name is Santiago Solana.</h1>
-            </div>
-            <ul className="nav justify-content-center">
-                <li className="nav-item">
-                    <a className="nav-link about"  href={props.link}>About</a>
-                </li>
+    <div className="card justify-content-center header-card">
+      <div className="typewriter">
+        <h1>Hello, my name is Santiago Solana.</h1>
+      </div>
+      <ul className="nav justify-content-center">
+        <Route
+          render={({ history }) => (
+            <li className="nav-item">
+              <a
+                className="nav-link about"
+                onClick={() => {
+                  history.push("/about");
+                }}
+                href={props.link}
+              >
+                About
+              </a>
+            </li>
+          )}
+        />
+        
+         <li className="nav-item">
+          <h1 className="divider">|</h1>
+        </li>
 
-                <li className="nav-item">
-                    <h1 className="divider">|</h1>
-                </li>
+        <Route
+          render={({ history }) => (
+            <li className="nav-item">
+              <a
+                className="nav-link about"
+                onClick={() => {
+                  history.push("/portfolio");
+                }}
+                href={props.link}
+              >
+                Portfolio
+              </a>
+            </li>
+          )}
+        />
+        
 
-                <li className="nav-item">
-                    <a className="nav-link portfolio"  href={props.link}>Portfolio</a>
-                </li>
+        <li className="nav-item">
+          <h1 className="divider">|</h1>
+        </li>
 
-                <li className="nav-item">
-                    <h1 className="divider">|</h1>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link contact"  href={props.link}>Contact</a>
-                </li>
-            </ul>
-        </div>
+        <li className="nav-item">
+          <a className="nav-link contact" href={props.link}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
