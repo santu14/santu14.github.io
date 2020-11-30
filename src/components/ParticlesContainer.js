2b278987fcadc7d380fc8a8062f4b74737c792d5
Santiago Solana <santiago.sjs@gmail.com>
2020-11-30 16:25:43 -0500

    particles change

--- src/components/ParticlesContainer.js
+++ src/components/ParticlesContainer.js
@@ -1,66 +1,66 @@
-import React from 'react'
+import React from "react";
 import Particles from "react-particles-js";
 
 function ParticlesContainer() {
-    return (
-        <div>
-            <Particles
-            className="particles"
-          height={900}
-          params={{
-            particles: {
-              number: {
-                value: 400,
-                density: {
-                  enable: false,
-                },
-              },
-              size: {
-                value: 1,
-                random: true,
-                anim: {
-                  speed: 1,
-                  size_min: 0.3,
-                },
-              },
-              line_linked: {
+  return (
+    <div>
+      <Particles
+        className="particles"
+        height={900}
+        params={{
+          particles: {
+            number: {
+              value: 400,
+              density: {
                 enable: false,
               },
-              move: {
-                random: true,
-                speed: .5,
-                direction: "top",
-                out_mode: "out",
+            },
+            size: {
+              value: 1,
+              random: true,
+              anim: {
+                speed: 1,
+                size_min: 0.3,
               },
             },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: "bubble",
-                },
-                onclick: {
-                  enable: true,
-                  mode: "repulse",
-                },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 0.5,
+              direction: "top",
+              out_mode: "out",
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble",
               },
-              modes: {
-                bubble: {
-                  distance: 250,
-                  duration: 2,
-                  size: 0,
-                  opacity: 0,
-                },
-                repulse: {
-                  distance: 400,
-                  duration: 4,
-                },
+              onclick: {
+                enable: true,
+                mode: "repulse",
               },
             },
-          }}
-        />
-        </div>
-    )
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
 
-export default ParticlesContainer
+export default ParticlesContainer;
